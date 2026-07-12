fefa6734ace7c9773f09f78ed7d856bb73304f3f
Nivish-21 <95935550+Nivish-21@users.noreply.github.com>
2026-07-12 13:24:52 +0530

    fix: commit generated convex bindings

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,57 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as dashboard from "../dashboard.js";
+import type * as ingest from "../ingest.js";
+import type * as requests from "../requests.js";
+import type * as tasks from "../tasks.js";
+import type * as trace from "../trace.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  dashboard: typeof dashboard;
+  ingest: typeof ingest;
+  requests: typeof requests;
+  tasks: typeof tasks;
+  trace: typeof trace;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
